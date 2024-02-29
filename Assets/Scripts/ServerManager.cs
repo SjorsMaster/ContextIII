1f1c209b22a0a380078e2f8d41c6af50821ae1c6
Yvar Toorop <yvarftoorop@hotmail.com>
2024-02-29 16:32:01 +0100

    Added server manager to host scene

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace ContextIII
+{
+    [RequireComponent(typeof(NetworkIdentity))]
+    public class ServerManager : NetworkBehaviour
+    {
+        [SerializeField] private Camera serverCamera;
+
+        [ServerCallback]
+        private void Start()
+        {
+            if (isServer)
+                serverCamera.gameObject.SetActive(true);
+        }
+    }
+}
