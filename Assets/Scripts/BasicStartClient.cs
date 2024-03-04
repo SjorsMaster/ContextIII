47c60156ba4632c8e335fbe92c2145192ab52e14
Yvar Toorop <yvarftoorop@hotmail.com>
2024-03-04 11:35:46 +0100

    Added easier connect function to client scene

--- Assets/Scripts/BasicStartClient.cs
+++ Assets/Scripts/BasicStartClient.cs
@@ -11,11 +11,13 @@ namespace ContextIII
         [SerializeField] private bool autoConnectOnStartup;
         [SerializeField] private NetworkManager networkManager;
         [SerializeField] string networkAddress;
-        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private TMP_Text[] ipTextFields;
 
         private void Start()
         {
-            inputField.text = networkAddress;
+            string[] networkAddressSplit = networkAddress.Split('.');
+            for (int i = 0; i < ipTextFields.Length; i++)
+                ipTextFields[i].text = networkAddressSplit[i];
 
             if (autoConnectOnStartup)
                 StartClient();
@@ -23,10 +25,30 @@ namespace ContextIII
 
         public void StartClient()
         {
-            if (!string.IsNullOrEmpty(networkAddress))
-                networkManager.networkAddress = networkAddress;
+            string ipAddress = "";
+            for (int i = 0; i < ipTextFields.Length; i++)
+            {
+                ipAddress += ipTextFields[i].text;
+                if (i < ipTextFields.Length - 1)
+                    ipAddress += ".";
+            }
+            networkManager.networkAddress = ipAddress;
 
             networkManager.StartClient();
+        }
+
+        public void IncreaseIpValue(TMP_Text textField)
+        {
+            int currentIpValue = int.Parse(textField.text);
+            currentIpValue++;
+            textField.text = currentIpValue.ToString();
+        }
+
+        public void DecreaseIpValue(TMP_Text textField)
+        {
+            int currentIpValue = int.Parse(textField.text);
+            currentIpValue--;
+            textField.text = currentIpValue.ToString();
         }
     }
 }
