00ad706eea330e190a9dbc966b5e55b8a7daad7e
Sjors Kaagman <sjorspnk@gmail.com>
2024-03-04 15:47:46 +0100

    some safety checks, and now you can modify the target (useful for multiplayer)

--- Assets/Table/FollowHand.cs
+++ Assets/Table/FollowHand.cs
@@ -9,22 +9,20 @@ public class FollowHand : MonoBehaviour
 
     Rigidbody _rigidbody;
 
-    void Start(){
-        _rigidbody = _paddle.GetComponent<Rigidbody>();
-    }
+    void Start() => UpdateTarget(_paddle.GetComponent<Rigidbody>());
+    public void UpdateTarget(GameObject target) => UpdateTarget(target.GetComponent<Rigidbody>());
+    public void UpdateTarget(Rigidbody target) => _rigidbody = target;
 
     // Update is called once per frame
-    void Update()
-    {
-        _rigidbody.MovePosition(CheckBounds(_hand.transform.position));
+    void Update(){
+        if(_rigidbody) _rigidbody.MovePosition(CheckBounds(_hand.transform.position));
     }
 
     Vector3 CheckBounds(Vector3 input){
-
         float tmp1 = input.x > _boundsA.transform.position.x ? (input.x < _boundsB.transform.position.x ? input.x : _boundsB.transform.position.x) : _boundsA.transform.position.x;
         float tmp2 = input.z > _boundsA.transform.position.z ? (input.z < _boundsB.transform.position.z ? input.z : _boundsB.transform.position.z) : _boundsA.transform.position.z;
 
-        print($"{tmp1},{input.y},{tmp2}");
+        //print($"{tmp1},{input.y},{tmp2}");
         return new Vector3(tmp1, input.y, tmp2);
     }
 }
