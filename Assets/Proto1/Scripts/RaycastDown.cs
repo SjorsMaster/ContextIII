c773c3cd439ada1bec8606102c02dcd1608c2641
Yvar Toorop <yvarftoorop@hotmail.com>
2024-04-09 14:52:57 +0200

    Fixed SjorsMasters tiny mistake

--- Assets/Proto1/Scripts/RaycastDown.cs
+++ Assets/Proto1/Scripts/RaycastDown.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class RaycastDown : MonoBehaviour
 {
-    public bool grounded { get; private set; }
-    public UnityEvent grounded, notGrounded;
+    public bool Grounded { get; private set; }
+
+    public UnityEvent onGrounded, onNotGrounded;
+
     void FixedUpdate()
     {
         // Bit shift the index of the layer (8) to get a bit mask
@@ -16,19 +16,18 @@ public class RaycastDown : MonoBehaviour
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
         layerMask = ~layerMask;
 
-        RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity, layerMask))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(-Vector3.up), out RaycastHit hit, Mathf.Infinity, layerMask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * hit.distance, Color.yellow);
-            grounded?.Invoke();
-            grounded = true;
+            onGrounded?.Invoke();
+            Grounded = true;
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * 1000, Color.white);
-            notGrounded?.Invoke();
-            grounded = false;
+            onNotGrounded?.Invoke();
+            Grounded = false;
         }
     }
 }
