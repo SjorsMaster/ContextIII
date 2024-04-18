2af5d80a8e8cfb7013ebd4e0f3bae10883455988
Sjors Kaagman <sjorspnk@gmail.com>
2024-04-18 12:38:31 +0200

    Build collection of scenes and a script to manage the rotary door, also some audio that's yet to be implemented and we also now use allskyfree

---
+++ Assets/Proto2_sjors/Scripts/DraaiDeurBV.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DraaiDeurBV : MonoBehaviour
+{
+    public List<UnityEvent> entryBehaviour, exitBehaviour;
+    int tracker=0, cooldownS = 3;
+    bool canTrigger = true, canExit = false; //To avoid repeat calling when moving back an forth
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(canTrigger == true){
+            canTrigger = false;
+            canExit = true;
+            if(entryBehaviour.Count >= tracker+1)entryBehaviour[tracker].Invoke();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(canExit == true){
+            canExit = false;
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        if(exitBehaviour.Count >= tracker+1)exitBehaviour[tracker].Invoke();
+        yield return new WaitForSeconds(cooldownS);
+        canTrigger = true;
+        tracker++;
+    }
+}
