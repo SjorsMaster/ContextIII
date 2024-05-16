5a79e8643dfaeeae357b06965a422f0c9dab69c1
Mac <yvarftoorop@hotmail.com>
2024-05-16 16:39:58 +0200

    Score counter examples

---
+++ Assets/Proto3/Scripts/ScoreCounter.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using System;
+using UnityEngine;
+
+public class ScoreCounter : NetworkBehaviour
+{
+    [SyncVar(hook = nameof(OnScoreUpdated))] private int score;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdIncrementScore();
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    private void CmdIncrementScore()
+    {
+        score++;
+    }
+
+    [Client]
+    private void OnScoreUpdated(int oldScore, int newScore)
+    {
+        Debug.Log($"Score updated: {oldScore} -> {newScore}");
+
+        // Do something with the new score
+    }
+
+
+}
