2235e87474b399b783eb6b7a763bf8ec668e1469
Yvar Toorop <yvarftoorop@hotmail.com>
2024-06-07 11:23:23 +0200

    Scene switching seems to work fine now

--- Assets/ColocatedStuff/PositionTracking/TrackingSaveDataManager.cs
+++ Assets/ColocatedStuff/PositionTracking/TrackingSaveDataManager.cs
@@ -1,29 +1,9 @@
 ﻿using Mirror;
 using SharedSpaces.SaveSystem;
-using UnityEngine;
 
 public class TrackingSaveDataManager : BaseSaveDataManager<TrackingSaveData>
 {
-    [SerializeField] private string TrackingDataFileName = "TrackingData";
-
     public override string FileNameSuffix { get; set; } = "TrackingData";
-
-    public override void SaveData()
-    {
-        SetFileName(TrackingDataFileName);
-        base.SaveData();
-    }
-    public override void LoadData()
-    {
-        SetFileName(TrackingDataFileName);
-        base.LoadData();
-    }
-
-    public override void NewData()
-    {
-        SetFileName(TrackingDataFileName);
-        base.NewData();
-    }
 
     public void VisualizeOnClients()
     {
